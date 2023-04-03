ec97ec03d6467db20c415ff134cc0bbc63ceff6a
HyunseoPark <sspp8585@icloud.com>
2023-04-03 14:03:18 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button sangButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+
+        sangButton = findViewById(R.id.taxi_sang);
+        sangButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
