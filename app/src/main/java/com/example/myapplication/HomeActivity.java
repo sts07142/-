4d5a8b3c2d0a0fa522fed7595da9dd108d4f4aed
HyunseoPark <sspp8585@icloud.com>
2023-04-03 15:04:01 +0900

    택보기 리스트 화면 생성

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomeActivity extends AppCompatActivity {
 
     private Button sangButton;
+    private Button bogiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,15 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bogiButton = findViewById(R.id.taxi_bogi);
+        bogiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, bogi_listActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/myapplication/bogi_listActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bogi_listActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bogi_list);
+    }
+}
